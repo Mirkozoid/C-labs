e752e2ca98c4d6e1ba2f1dec24b2b7e3e1882b23
Mirkozoid <rudanskiu@list.ru>
2022-10-03 14:11:41 +0300

    refact

--- piatnashki/Program.cs
+++ piatnashki/Program.cs
@@ -4,127 +4,104 @@ namespace piatnashki
 {
     class Program
     {
-
-        static void Main(string[] args)
+        static void FieldOutput(int[,] field)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[i, j] + "\t");
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine();
+            }
+        }
+        static void Main()
         {
             while (true)
             {
                 int[,] field = new int[4, 4];
                 Random random = new Random();
                 int[] numbers = new int[16];
-                int ind = 0;
-                int a = 0;
+                int initialNumber = 0;
                 for (int i = 0; i < numbers.Length; i++)
                 {
-
-
-                    numbers[i] = a;
-                    a++;
+                    numbers[i] = i;
                 }
                 for (int i = numbers.Length - 1; i >= 1; i--)
                 {
                     int j = random.Next(i + 1);
-                    var temp = numbers[j];
+                    var replacement = numbers[j];
                     numbers[j] = numbers[i];
-                    numbers[i] = temp;
-                }
-                for (int i = 0; i < field.GetLength(0); i++)
-                {
-
-                    for (int j = 0; j < field.GetLength(1); j++)
-                    {
-
-                        field[i, j] = numbers[ind];
-                        ind++;
-                    }
+                    numbers[i] = replacement;
                 }
                 for (int i = 0; i < field.GetLength(0); i++)
                 {
                     for (int j = 0; j < field.GetLength(1); j++)
                     {
-                        Console.Write(field[i, j] + "\t");
+                        field[i, j] = numbers[initialNumber];
+                        initialNumber++;
                     }
-                    Console.WriteLine(" ");
-                    Console.WriteLine();
                 }
+                FieldOutput(field);
                 Console.ReadKey();
                 while (true)
                 {
                     Console.WriteLine("select the number you want to change from 0");
                     if (int.TryParse(Console.ReadLine(), out int number))
                     {
-                        int index = 0;
-                        int ger = 0;
-                        int newIndex = 0;
-                        int newGer = 0;
+                        int firstIndexEmptyField = 0;
+                        int secondIndexEmptyField = 0;
+                        int firstIndexSelectedNumber = 0;
+                        int secondIndexSelectedNumber = 0;
                         for (int i = 0; i < 4; i++)
                         {
-                            for (int g = 0; g < 4; g++)
+                            for (int j = 0; j < 4; j++)
                             {
-                                if (field[i, g] == 0)
+                                if (field[i, j] == 0)
                                 {
-                                    index = i;
-                                    ger = g;
-                                    for (int l = 0; l < 4; l++)
-                                    {
-                                        for (int j = 0; j < 4; j++)
-                                        {
-
-                                            if (field[l, j] == number)
-                                            {
-                                                newIndex = l;
-                                                newGer = j;
-                                            }
-                                        }
-                                    }
+                                    firstIndexEmptyField = i;
+                                    secondIndexEmptyField = j;                                   
                                 }
                             }
                         }
-
-                        if (newIndex == index)
+                        for (int l = 0; l < 4; l++)
                         {
-                            if (newGer < ger + 2 && newGer > ger - 2)
+                            for (int j = 0; j < 4; j++)
                             {
-                                Console.WriteLine("rr");
-                                for (int p = 0; p < 1; p++)
+                                if (field[l, j] == number)
                                 {
-                                    var temp = field[index, ger];
-                                    field[index, ger] = field[newIndex, newGer];
-                                    field[newIndex, newGer] = temp;
-                                }
-                                Console.Clear();
-                                for (int k = 0; k < field.GetLength(0); k++)
-                                {
-                                    for (int o = 0; o < field.GetLength(1); o++)
-                                    {
-                                        Console.Write(field[k, o] + "\t");
-                                    }
-                                    Console.WriteLine(" ");
-                                    Console.WriteLine();
+                                    firstIndexSelectedNumber = l;
+                                    secondIndexSelectedNumber = j;
                                 }
                             }
                         }
-                        if (newGer == ger)
+                        if (firstIndexSelectedNumber == firstIndexEmptyField)
                         {
-                            if (newIndex < index + 2 && newIndex > index - 2)
+                            if (secondIndexSelectedNumber < secondIndexEmptyField + 2 && secondIndexSelectedNumber > secondIndexEmptyField - 2)
                             {
-                                Console.WriteLine("rr");
                                 for (int p = 0; p < 1; p++)
                                 {
-                                    var temp = field[index, ger];
-                                    field[index, ger] = field[newIndex, newGer];
-                                    field[newIndex, newGer] = temp;
+                                    var replacement = field[firstIndexEmptyField, secondIndexEmptyField];
+                                    field[firstIndexEmptyField, secondIndexEmptyField] = field[firstIndexSelectedNumber, secondIndexSelectedNumber];
+                                    field[firstIndexSelectedNumber, secondIndexSelectedNumber] = replacement;
                                 }
                                 Console.Clear();
-                                for (int k = 0; k < field.GetLength(0); k++)
+                                FieldOutput(field);
+                            }
+                        }
+                        if (secondIndexSelectedNumber == secondIndexEmptyField)
+                        {
+                            if (firstIndexSelectedNumber < firstIndexEmptyField + 2 && firstIndexSelectedNumber > firstIndexEmptyField - 2)
+                            {
+                                for (int p = 0; p < 1; p++)
                                 {
-                                    for (int o = 0; o < field.GetLength(1); o++)
-                                    {
-                                        Console.Write(field[k, o] + "\t");
-                                    }
-                                    Console.WriteLine(" ");
-                                    Console.WriteLine();
+                                    var replacement = field[firstIndexEmptyField, secondIndexEmptyField];
+                                    field[firstIndexEmptyField, secondIndexEmptyField] = field[firstIndexSelectedNumber, secondIndexSelectedNumber];
+                                    field[firstIndexSelectedNumber, secondIndexSelectedNumber] = replacement;
                                 }
+                                Console.Clear();
+                                FieldOutput(field);
                             }
                         }
                     }
@@ -133,26 +110,10 @@ namespace piatnashki
                         Console.WriteLine("Error");
                         Console.ReadKey();
                         Console.Clear();
-
-                        for (int i = 0; i < field.GetLength(0); i++)
-                        {
-                            for (int j = 0; j < field.GetLength(1); j++)
-                            {
-                                Console.Write(field[i, j] + "\t");
-                            }
-                            Console.WriteLine(" ");
-                            Console.WriteLine();
-
-                        }
-                        Console.ReadKey();
-                        
+                        FieldOutput(field);
+                        Console.ReadKey();                        
                     }
-
-
-
                 }
-
-
             }
         }
     }
