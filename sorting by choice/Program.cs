83fef749a9db04e598dc030cad7a68e140319eb5
Mirkozoid <rudanskiu@list.ru>
2022-10-14 11:43:56 +0300

    frst

---
+++ sorting by choice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace sorting_by_choice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements array");
+            int numberElements = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numberElements];
+            int temp;
+            int min;
+            Random randomElement = new Random();
+            for (int i = 0; i < numberElements; i++)
+            {
+                array[i] = randomElement.Next(1, 150);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numberElements; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;                       
+                    }
+                }
+                temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+            for (int i = 0; i < numberElements; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sorting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace bubble_sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements array");
+            int numberElements = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numberElements];
+            int temp;
+            Random randomElement = new Random(); 
+            for (int i = 0; i < numberElements; i++)
+            {
+                array[i] = randomElement.Next(1, 150);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numberElements; i++)
+            {
+                for (int j = 0; j < array.Length-1-i; j++)
+                {
+                   if (array[j] > array[j + 1])
+                   {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;                        
+                   }
+                }                   
+            }
+            for (int i = 0; i < numberElements; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
